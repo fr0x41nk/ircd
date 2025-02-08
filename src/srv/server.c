3d6770183b7e18a6482654f4b2736e304e5bcda6
Frank Solli <frank@frank2.net>
2025-02-08 12:43:25 +0100

    init

--- src/srv/server.c
+++ src/srv/server.c
@@ -1,1 +1,43 @@
-#include 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
+
+
+int main() {
+
+    struct sockaddr_in serverInfo = {0};
+    struct sockaddr_in clientInfo = {0};
+    
+
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
+        perror("Socket");
+        return -1;
+    }
+
+    //bind
+
+    if (bind(fd, (struct sockaddr *)&serverInfo, sizeof(serverInfo)) == -1) {
+        perror("bind");
+        close(fd);
+        return -1;
+    }
+
+    //listen
+
+    if (listen(fd,0) == -1) {
+        perror("Listen");
+        close(fd);
+        return -1;
+    }
+
+    
+
+
+
+}
+
